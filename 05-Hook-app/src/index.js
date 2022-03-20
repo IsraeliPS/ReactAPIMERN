4eecae64b80e9a499406c6c377aeff73b49a1b0a
Israeli Perez <israeli.ps@gmail.com>
2022-03-20 17:51:53 -0600

    practicando con hooks

--- 05-Hook-app/src/index.js
+++ 05-Hook-app/src/index.js
@@ -1,6 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { Layout } from './components/05-useLayoutEffect/Layout'
+// import { CallbackHook } from './components/06-memos/CallbackHook'
+import { Padre } from './components/07-tarea-memo/Padre'
+// import { MemoHook } from './components/06-memos/MemoHook'
+// import { Memorize } from './components/06-memos/Memorize'
+// import { Layout } from './components/05-useLayoutEffect/Layout'
 // import { RealExampleRef } from './components/04-useRef/RealExampleRef'
 // import { FocusScreen } from './components/04-useRef/FocusScreen'
 // import { MultipleCustomHooks } from './components/03-examples/MultipleCustomHooks'
@@ -8,7 +12,7 @@ import { Layout } from './components/05-useLayoutEffect/Layout'
 // import { SimpleForm } from './components/02-UseEffect/SimpleForm'
 
 ReactDOM.render(
-  <Layout />,
+  <Padre />,
   document.getElementById('root')
 
 )
