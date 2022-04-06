9dc7b93998219a350f20817b1b80bbd7e894cbf5
Israeli Perez <israeli.ps@gmail.com>
2022-04-06 15:57:43 -0500

    pruebas en useContext

---
+++ 05-Hook-app/src/tests/components/09useContext/AppRouter.test.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { AppRouter } from '../../../components/09-useContext/AppRouter'
+import { mount } from 'enzyme'
+
+describe('Pruebas en <AppRouter />', () => {
+  const wrapper = mount(
+    <AppRouter />
+  )
+
+  test('Debe de mostrarse correctamente', () => {
+
+  })
+})
