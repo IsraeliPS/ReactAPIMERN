8fb67edcefa2ac72a078b8e3825d51a012c98d24
Israeli Perez <israeli.ps@gmail.com>
2022-04-07 16:40:41 -0500

    end testing Hooks

--- 05-Hook-app/src/tests/components/09useContext/AppRouter.test.js
+++ 05-Hook-app/src/tests/components/09useContext/AppRouter.test.js
@@ -1,13 +1,20 @@
 import React from 'react'
 import { AppRouter } from '../../../components/09-useContext/AppRouter'
 import { mount } from 'enzyme'
+import { UserContext } from '../../../components/09-useContext/UserContext'
 
 describe('Pruebas en <AppRouter />', () => {
+  const user = {
+    id: 1,
+    name: 'israel'
+  }
   const wrapper = mount(
-    <AppRouter />
+    <UserContext.Provider value={user}>
+      <AppRouter />
+    </UserContext.Provider>
   )
 
   test('Debe de mostrarse correctamente', () => {
-
+    expect(wrapper).toMatchSnapshot()
   })
 })
